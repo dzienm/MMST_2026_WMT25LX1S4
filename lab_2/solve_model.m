c17c0e7eac7d0d300960b60f073c996c6767bcd1
dellpad_MD\mdziendzikowski <m.dziendzikowski@gmail.com>
2026-04-10 12:24:28 +0200

    Rozwiązanie 1.

---
+++ lab_2/solve_model.m
@@ -0,0 +1,19 @@
+function [t, x] = solve_model(A, x0, t_span)
+    % solve_model - Ścisłe rozwiązanie liniowego układu równań x' = Ax
+    %
+    % Argumenty:
+    % A      - macierz układu (2x2)
+    % x0     - wektor warunków początkowych [x1_0; x2_0]
+    % t_span - zakres czasu symulacji, np. [0, 10]
+    
+    % Wygenerowanie wektora czasu (1000 punktów dla gładkiego wykresu)
+    t = linspace(t_span(1), t_span(2), 1000);
+    
+    % Inicjalizacja macierzy na wyniki
+    x = zeros(2, length(t));
+    
+    % Obliczenie ścisłego rozwiązania z wykorzystaniem macierzy eksponencjalnej
+    for i = 1:length(t)
+        x(:, i) = expm(A * t(i)) * x0;
+    end
+end
